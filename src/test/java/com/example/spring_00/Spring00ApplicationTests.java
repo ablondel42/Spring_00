dbbb7ec5929c1ec8268e84fc831b2d585e5c1f22
ablondel42 <ablondel42@icloud.com>
2024-07-13 19:20:28 +0200

    init

---
+++ src/test/java/com/example/spring_00/Spring00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
